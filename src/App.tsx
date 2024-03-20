b2e7a45b41fe245b207c51ff162aadca31be5f18
unknown <aliaksei.tsiatsiokin@appvelocity.io>
2024-03-20 17:24:07 +0100

    add variables names into css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+
+// import './temporary.css';
+
+import style from './App.module.css';
+
+const items = [{
+    text: 'actionPrimaryActive',
+    varName: '--light-theme-mode-1-action-primary-active',
+  }, {
+      text: 'actionPrimaryHover',
+      varName: '--light-theme-mode-1-action-primary-hover',
+  },{
+    text: 'actionSecondaryDefault',
+    varName: '--light-theme-mode-1-action-secondary-default',
+  }, {
+    text: 'actionSecondaryHover',
+    varName: '--light-theme-mode-1-action-secondary-hover',
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={style.root}>
+        <Grid container spacing={2} sx={{ flexWrap: 'nowrap' }}>
+          <Grid item xs={6} className={style.grid}>
+            <h2>Variables preview</h2>
+            {items.map(
+                ({ text, varName }) => (
+                <div className={style.row}>
+                  <div
+                    className={style.square}
+                    style={{
+                      backgroundColor: `var(${varName})`
+                    }}
+                  />
+                  <span>{text}</span>
+                </div>
+              )
+            )}
+          </Grid>
+          <Grid item xs={6} className={style.grid}>
+            <h2>Components</h2>
+            <Button className={style.primary_button} variant="contained">Button Primary</Button>
+            <Button className={style.secondary_button} variant="contained">Button Secondary</Button>
+          </Grid>
+        </Grid>
+      </div>
   );
 }
 
