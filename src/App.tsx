cdd13f8e2c9146ae86cca949ada257101074db6f
Marcin Grygierek <marcin.grygierek@appvelocity.io>
2024-07-17 11:00:04 +0200

    Added craco to handle module custom transpilation, a bit of refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React, { useMemo } from 'react';
 
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Grid from '@mui/material/Grid';
 
 import style from './App.module.css';
 
-// import '@app-velocity/playground-fifteen-july/style.css';
+import '@app-velocity/playground-fifteen-july/common-styles.css';
+import '@app-velocity/playground-fifteen-july/common-styles-var.css';
 
-// import { playgroundColorVariable, variables } from '@app-velocity/playground-fifteen-july';
+// import { variables } from '@app-velocity/playground-fifteen-july/variables';
+// import { playgroundColorVariable } from '@app-velocity/playground-fifteen-july/index';
 
 // ---- comment following block when using import from published package ------
-const playgroundColorVariable = '#fff', variables = {
+const playgroundColorVariable = '#fff';
+const variables = {
   playgroundCollection: {
     Mode1: {
       playgroundColorVariable: '',
@@ -26,6 +28,8 @@ const playgroundColorVariable = '#fff', variables = {
 function App() {
   const [value, setValue] = React.useState<number>(0);
 
+  console.log('Variables', variables);
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -33,125 +37,110 @@ function App() {
   return useMemo(() => (
     <div className={style.root}>
       <h2>Variables preview</h2>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="Basic tabs example"
-        >
-          <Tab label="CSS" />
-          <Tab label="JS" />
-        </Tabs>
-        {value === 0 ? (
-        <Grid container>
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>--playground-color-style-background:</div>
-              <div
-                  className={style.square}
-                  style={{
-                    backgroundColor: `var(--playground-color-style-background)`
-                  }}
-              />
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="Basic tabs example"
+      >
+        <Tab label="CSS" />
+        <Tab label="JS" />
+      </Tabs>
+      {value === 0 ? (
+        <>
+          <div className={style.row}>
+            <div>--playground-color-style-background:</div>
+            <div
+              className={style.square}
+              style={{
+                backgroundColor: `var(--playground-color-style-background)`
+              }}
+            />
+          </div>
+
+          <div className={style.row}>
+            <div>--content-dark-background:</div>
+            <div
+              className={style.square}
+              style={{
+                backgroundColor: `var(--content-dark-background)`
+              }}
+            />
+          </div>
+
+          <div className={style.row}>
+            <div>--dark-grey-04-background:</div>
+            <div
+              className={style.square}
+              style={{
+                backgroundColor: `var(--dark-grey-04-background)`
+              }}
+            />
+          </div>
+
+          <div className={style.row}>
+            <div>--general-black-111111-background:</div>
+            <div
+              className={style.square}
+              style={{
+                backgroundColor: `var(--general-black-111111-background)`
+              }}
+            />
+          </div>
+
+          <div className={style.row}>
+            <div>className="playground-text-style":</div>
+            <div className="playground-text-style">
+              The quick brown fox jumps over the lazy dog
             </div>
-          </Grid>
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>--content-dark-background:</div>
-              <div
-                  className={style.square}
-                  style={{
-                    backgroundColor: `var(--content-dark-background)`
-                  }}
-              />
+          </div>
+
+          <div className={style.row}>
+            <div>.playground-effect-style:</div>
+            <div className="effect_preview_image">
+              <div className="effect_preview_bg_1"></div>
+              <div className="effect_preview_bg_2"></div>
+              <div className="effect_preview_bg_3"></div>
+              <div className={`effect_component .playground-effect-style`}></div>
             </div>
-          </Grid>
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>--dark-grey-04-background:</div>
-              <div
-                  className={style.square}
-                  style={{
-                    backgroundColor: `var(--dark-grey-04-background)`
-                  }}
-              />
-            </div>
-          </Grid>
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>--general-black-111111-background:</div>
-              <div
-                  className={style.square}
-                  style={{
-                    backgroundColor: `var(--general-black-111111-background)`
-                  }}
-              />
-            </div>
-          </Grid>
-          <Grid item xs={12} className={style.grid}>
-            <div className={style.row}>
-              <div>className="playground-text-style":</div>
-              <div className="playground-text-style">
-                The quick brown fox jumps over the lazy dog
-              </div>
-            </div>
-          </Grid>
-          <Grid item xs={12} className={style.grid}>
-            <div className={style.row}>
-              <div>.playground-effect-style:</div>
-              <div className="effect_preview_image">
-                <div className="effect_preview_bg_1"></div>
-                <div className="effect_preview_bg_2"></div>
-                <div className="effect_preview_bg_3"></div>
-                <div className={`effect_component .playground-effect-style`}></div>
-              </div>
-            </div>
-          </Grid>
-        </Grid>
-        ) : null}
+          </div>
+        </>
+      ) : null}
 
 
 
-        {/* JS part */}
+      {/* JS part */}
 
 
 
-        {value === 1 ? (
-        <Grid container>
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>CommonStyles?.colors?['contentDark.background']</div>
-              <div
-                  className={style.square}
-                  style={{
-                    backgroundColor: `${playgroundColorVariable}`
-                  }}
-              />
-            </div>
-          </Grid>
+      {value === 1 ? (
+        <>
+          <div className={style.row}>
+            <div>CommonStyles?.colors?['contentDark.background']</div>
+            <div
+              className={style.square}
+              style={{
+                backgroundColor: `${playgroundColorVariable}`
+              }}
+            />
+          </div>
 
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>variables?.playgroundCollection?.Mode1?.playgroundNumberVariable</div>
-              <div>{variables?.playgroundCollection?.Mode1?.playgroundNumberVariable}</div>
-            </div>
-          </Grid>
+          <div className={style.row}>
+            <div>variables?.playgroundCollection?.Mode1?.playgroundNumberVariable</div>
+            <div>{variables?.playgroundCollection?.Mode1?.playgroundNumberVariable}</div>
+          </div>
 
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>variables?.playgroundCollection?.Mode1?.playgroundTextVariable</div>
-              <div>{variables?.playgroundCollection?.Mode1?.playgroundTextVariable}</div>
-            </div>
-          </Grid>
+          <div className={style.row}>
+            <div>variables?.playgroundCollection?.Mode1?.playgroundTextVariable</div>
+            <div>{variables?.playgroundCollection?.Mode1?.playgroundTextVariable}</div>
+          </div>
 
-          <Grid item xs={3} className={style.grid}>
-            <div className={style.row}>
-              <div>variables?.playgroundCollection?.Mode1?.playgroundBooleanVariable</div>
-              <div>{variables?.playgroundCollection?.Mode1?.playgroundBooleanVariable ? 'true' : 'false'}</div>
-            </div>
-          </Grid>
-        </Grid>
-        ): null}
-      </div>
+          <div className={style.row}>
+            <div>variables?.playgroundCollection?.Mode1?.playgroundBooleanVariable</div>
+            <div>{variables?.playgroundCollection?.Mode1?.playgroundBooleanVariable ? 'true' : 'false'}</div>
+          </div>
+        </>
+      ) : null}
+    </div>
   ), [value]);
 }
 
